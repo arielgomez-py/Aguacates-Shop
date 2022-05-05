4ac730b912b08df32c04e677adeb5b2ad91675da
Ariel <arielgomezmendez94@gmail.com>
2022-05-04 22:09:49 -0400

    First commit

--- src/index.js
+++ src/index.js
@@ -3,4 +3,44 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const baseurl  = "https://platzi-avo.vercel.app";
+
+const formatPrice = (price) => {
+    const newPrice = new window.Intl.NumberFormat('en-EN', {
+        style: "currency",
+        currency:"USD",
+    }).format(price);
+    return newPrice;
+}
+
+//Web api
+//Conectar al servidor
+window
+    .fetch(`${baseurl}/api/avo`)
+    //procesar la respuesta y convertirla en JSON
+    .then((respuesta) => respuesta.json())
+    //renderizar la info en el navegador
+    .then(responseJson => {
+        const todosLosItems = [];
+        responseJson.data.forEach((item) => {
+            //crear imagen
+            const image = document.createElement('img');
+            image.src = `${baseurl}${item.image}`;
+  
+            //crear titulo
+            const title = document.createElement('h2');
+            title.textContent = item.name;
+            title.style.fontSize = "3rem";
+
+            //crear precio
+            const price = document.createElement('div');
+            price.textContent = formatPrice(item.price);
+
+            const container = document.createElement('div');
+            container.append(image, title, price)
+
+            todosLosItems.push(container);
+        });
+
+        document.body.append(... todosLosItems);
+    }) 
